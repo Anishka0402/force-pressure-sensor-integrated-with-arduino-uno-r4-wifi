4fb378281f137e1855a3a6dd0833f394a94ca4f0
Anishka0402 <agmaqueen446@gmail.com>
2026-02-10 10:02:47 +0530

    Add FSR reading and serial output to main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <Arduino.h>
+int fsrPin = A0; // the FSR and 10K pulldown are connected to a0
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
 }
 
 void loop() {
 // write your code here
+    int fsr = analogRead(fsrPin);
+    Serial.print("Analog reading = ");
+    Serial.println(fsr);
+    delay(300);
 }
